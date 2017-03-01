f3d25ee52d44222cd90ca35ed0c9caf1e0d0c96f
chenaisheng <4893310@qq.com>
2017-03-01 23:03:57 +0800

    pgclusteradmin

--- pgclusteradmin.go
+++ pgclusteradmin.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+   "net/http"
+   "fmt"
+   
+)
+
 func main() {
-     fmt.Println("hello pgclusteradmin");
+     //设置静态资源存入路径
+     http.Handle("/", http.FileServer(http.Dir("./easyui/")))
+     http.ListenAndServe(":10001", nil)
 }
